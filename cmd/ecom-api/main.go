1f24103877ec8e52437ddbb761209c060032c1e9
Andy Fusniak <andy@andyfusniak.com>
2019-09-27 12:31:18 +0100

    Bump verion

--- cmd/ecom-api/main.go
+++ cmd/ecom-api/main.go
@@ -35,7 +35,7 @@ import (
 	"google.golang.org/api/option"
 )
 
-var version = "v0.58.1"
+var version = "v0.59.0"
 
 const maxDbConnectAttempts = 3
 
--- schemas/postgres/schema/schema_version_function.sql
+++ schemas/postgres/schema/schema_version_function.sql
@@ -2,6 +2,6 @@ create function schema_version()
   returns text
 as
 $$
-  select 'v0.58.1' || '';
+  select 'v0.59.0' || '';
 $$
 language sql;
